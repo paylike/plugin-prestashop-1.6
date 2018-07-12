f52ea7cc05d546b3f8be792cd00d7658fcd56905
Calara Ionut <ionut.calara@gmail.com>
2018-07-12 17:43:20 +0300

    Update title escape to allow more characters

--- paylikepayment/paylikepayment.php
+++ paylikepayment/paylikepayment.php
@@ -4,7 +4,7 @@
  * @author    DerikonDevelopment <ionut@derikon.com>
  * @copyright Copyright (c) permanent, DerikonDevelopment
  * @license   Addons PrestaShop license limitation
- * @version   1.0.2
+ * @version   1.0.3
  * @link      http://www.derikon.com/
  *
  */
@@ -26,7 +26,7 @@ class PaylikePayment extends PaymentModule {
 	public function __construct() {
 		$this->name       = 'paylikepayment';
 		$this->tab        = 'payments_gateways';
-		$this->version    = '1.0.2';
+		$this->version    = '1.0.3';
 		$this->author     = 'DerikonDevelopment';
 		$this->bootstrap  = true;
 		$this->module_key = '1d083bab290f652fb6fb7ae35f9f0942';
